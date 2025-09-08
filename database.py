770e24719b810a136e49d3cde1ecf3644334a4e1
manasvig23 <manasvi.gawde23@gmail.com>
2025-09-08 19:58:51 +0530

    updating database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:root123@host.docker.internal:5432/abuse_moderation"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:root123@localhost:5432/abuse_moderation"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
